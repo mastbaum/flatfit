6f33d9e9224531f08db9e9239ca8eb1a76ef7108
Andy <amastbaum@gmail.com>
2011-05-18 14:12:40 -0400

    refactored to use event and pmt objects, added absorption and scattering

--- flatfit.py
+++ flatfit.py
@@ -10,14 +10,15 @@
 import sys
 import math, random
 import numpy
-from scipy import special as sp
 from matplotlib import pyplot as plt
 from matplotlib import cm, colors
 from mpl_toolkits.mplot3d import Axes3D
 from pylab import meshgrid
 
 npmts = 10
-radius = 1
+radius = 1.0
+attenuation_length = 10
+scattering_length = 10
 debug = False
 debug_hardcore = False
 
@@ -29,11 +30,18 @@ def pick_charge(mu=1.0, sigma=1.0):
   q = random.gauss(mu, sigma)
   return max(0.0, q)
 
-def plot_hit_distribution_polar(hits, pos=None):
-  npmts = len(hits)
+def plot_hit_distribution_polar(ev, pos=None):
+  '''polar plot of hit distribution for a single event'''
+  npmts = len(ev.pmts)
   fig1 = plt.figure(figsize=(2,2), facecolor='white')
   ax = fig1.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
   theta = numpy.arange(0, 2*math.pi, 2*math.pi/npmts)
+
+  # total number of hits in each tube
+  hits = [0] * npmts
+  for i in range(len(ev.pmts)):
+    hits[i] += ev.pmts[i].npe
+
   radii = numpy.array(hits)
   width = 2*numpy.pi/npmts
   bars = ax.bar(theta, radii, width=width, bottom=0.0)
@@ -41,16 +49,49 @@ def plot_hit_distribution_polar(hits, pos=None):
     bar.set_facecolor( cm.jet(r/float(max(hits))))
     bar.set_alpha(0.5)
   title = 'Distribution of PMT hits'
-  if position is not None: title += (', ' + str(pos))
+  if pos is not None: title += (', ' + str(pos))
   plt.title(title)
 
-def plot_hit_distribution(hits):
-  pmts = numpy.arange(0, len(hits))
-  fig2 = plt.figure(figsize=(2,2), facecolor='white')
-  bar = plt.bar(pmts, hits)
-  plt.title('Distribution of PMT hits')
-  plt.xlabel('PMT ID')
-  plt.ylabel('NHits')
+def histogram_nhit(events):
+  nevents = len(events)
+  npmts = len(events[0].pmts)
+  nhit = [0] * len(events)
+  for e in range(nevents):
+    for p in range(len(ev.pmts)):
+      if ev.pmts[p].npe > 0:
+        nhit[e] += 1
+  # apparent bug in matplotlib.
+  # if all values are the same (here 50), the bin is incorrectly labeled as 0.0
+  nhit.append(0.0)
+  f = plt.figure(figsize=(2,2), facecolor='white')
+  h = plt.hist(nhit, bins=100, log=False)
+  plt.title('Nhit distribution')
+  plt.xlabel('Number of incident photons')
+  plt.ylabel('Counts')
+
+def histogram_q(events):
+  npmts = len(events[0].pmts)
+  nhit = [0] * npmts
+  for ev in events:
+    for i in range(len(ev.pmts)):
+      nhit[i] += ev.pmts[i].q/len(events)
+  f = plt.figure(figsize=(2,2), facecolor='white')
+  h = plt.hist(nhit, bins=100, log=False)
+  plt.title('Charge distribution')
+  plt.xlabel('Total charge in PMT')
+  plt.ylabel('Counts')
+
+#def histogram_qpe(events):
+#  npmts = len(events[0].pmts)
+#  nhit = [0] * npmts
+#  for ev in events:
+#    for i in range(len(ev.pmts)):
+#      nhit[i] += (ev.pmts[i].q/ev.pmts[i].npe)/len(events)
+#  f = plt.figure(figsize=(2,2), facecolor='white')
+#  h = plt.hist(nhit, bins=100, log=False)
+#  plt.title('Charge distribution')
+#  plt.xlabel('Total charge in PMT')
+#  plt.ylabel('Counts')
 
 def histogram_mean_pe(l, pmtid):
   fig3 = plt.figure(figsize=(2,2), facecolor='white')
@@ -115,130 +156,169 @@ def q_vs_t_all(q, t, pos=None):
     ax.set_ybound(0,2)
     i += 1
 
-def simulate(nevents, pos, photons_per_event):
-  # initialize results arrays
-  hits = [0] * npmts
-  q = []
-  t = []
-  for i in range(npmts):
-    q.append([])
-    t.append([])
+class PMT:
+  '''holds a pmt's total charge, hit time, and number of photoelectrons for a
+  single event.
+  '''
+  def __init__(self):
+    self.npe = 0
+    self.q = 0.0
+    self.t = 0.0
+  def add_hit(self, nphotons, charge, time):
+    self.npe = npe
+    self.q = charge
+    self.t = time
 
-  # event loop
-  for event in range(nevents):
-    if (event % 10 == 0):
-      print '.',
-      sys.stdout.flush()
-    if (event == nevents - 1): print
-    if debug:
-      print '== event',event,'===='
-    if position is 'random':
-      event_r = radius + 1
-      while event_r > radius:
-        event_x = radius*(2*random.random()-1)
-        event_y = radius*(2*random.random()-1)
-        event_r = math.sqrt(event_x**2 + event_y**2)
-    else:
-      event_x = position[0]
-      event_y = position[1]
-      event_r = math.sqrt(event_x**2 + event_y**2)
-
-    # loop over this event's photons
-    q_temp = []
-    t_temp = []
-    hits_temp = []
+class Event:
+  '''holds data for a single event, including position and a list of PMTS'''
+  def __init__(self):
+    self.pos = None
+    self.pmts = []
     for i in range(npmts):
-      q_temp.append(0.0)
-      t_temp.append(0.0)
-      hits_temp.append(0)
-    for photon in range(photons_per_event):
-      if debug: print '-- photon', photon, '----'
-      theta = random.vonmisesvariate(0, 0)
-      if position == (0.0, 0.0):
-        ray_len = radius
-        angle_to_pmt = theta - math.pi
+      self.pmts.append(PMT())
+
+def get_hit_pmt_position(position):
+  '''given a photon position, picks a random momentum and find the distance to
+  the pmt it will hit, and the angle (in detector coordinates) to that pmt.
+  '''
+  event_x = position[0]
+  event_y = position[1]
+  event_r = math.sqrt(event_x**2 + event_y**2)
+  theta = random.vonmisesvariate(0, 0)
+  if position == (0.0, 0.0, 0.0):
+    ray_len = radius
+    angle_to_pmt = theta - math.pi
+  else:
+    a = 2.0 * (event_x*math.cos(theta) + event_y*math.sin(theta))
+    ray_len = -a/2 + 0.5 * math.sqrt(a**2 - 4*(event_r**2 - radius**2))
+    pmt_x = ray_len*math.cos(theta) + event_x
+    pmt_y = ray_len*math.sin(theta) + event_y
+    angle_to_pmt = math.atan2(pmt_y, pmt_x)
+
+  return ray_len, theta, angle_to_pmt
+
+def simulate(position, photons_per_event):
+  '''simulate propagates optical photons to pmts.
+
+  input: position: either an (x,y) tuple or 'random'. the latter will result in
+                   a uniformly-distributed random location inside radius.
+
+         photons_per_event: number of photons to throw at this location, cf.
+                            a 'photon bomb'
+
+  output: returns a Event including the simulated position and a list of PMT
+          objects (including unhit PMTs).
+  '''
+  event = Event()
+  if position is 'random':
+    event_r = radius + 1
+    while event_r > radius:
+      event_x = radius*(2*random.random()-1)
+      event_y = radius*(2*random.random()-1)
+  else:
+    event_x = position[0]
+    event_y = position[1]
+
+  track_x = []
+  track_y = []
+
+  # loop over this event's photons
+  for photon in range(photons_per_event):
+    pos = (event_x, event_y)
+
+    event_r = math.sqrt(event_x**2 + event_y**2)
+    if debug: print 'photon', photon
+    if debug: print ' start',pos,'r =',event_r
+    track_x.append(event_x)
+    track_y.append(event_y)
+
+    reached_pmt = False
+    kill_photon = False
+    while not reached_pmt:
+      ray_len, theta, angle_to_pmt = get_hit_pmt_position(pos)
+      # scattering (isotropic reemission)
+      if abs(ray_len) < random.expovariate(1.0/scattering_length):
+        # absorption
+        if abs(ray_len) > random.expovariate(1.0/attenuation_length):
+          kill_photon = True
+          break
+        reached_pmt = True
       else:
-        a = 2.0 * (event_x*math.cos(theta) + event_y*math.sin(theta))
-        ray_len = -a/2 - 0.5 * math.sqrt(a**2 - 4*(event_r**2 - radius**2))
-        global_x = ray_len*math.cos(theta) + event_x
-        global_y = ray_len*math.sin(theta) + event_y
-        angle_to_pmt = math.atan2(global_y, global_x)
+        scattering_distance = random.uniform(0, abs(ray_len)) # uniform?
+        # absorption before it can scatter?
+        if abs(scattering_distance) > random.expovariate(1.0/attenuation_length):
+          kill_photon = True
+          break
+        new_x = pos[0] + scattering_distance * math.cos(theta)
+        new_y = pos[1] + scattering_distance * math.sin(theta)
+        new_r = math.sqrt(new_x**2 + new_y**2)
+        if debug: print ' scattered', pos,'r =',new_r
+        if debug_hardcore:
+          print '  abs(ray_len):',abs(ray_len)
+          print '  scattering_dist:',scattering_distance
+          print '  theta',theta
+          print '  pos[0]:',pos[0]
+          print '  pos[1]:',pos[1]
+          print '  new_x:',new_x
+          print '  new_y:',new_y
+          print '  new_r:',new_r
+          if new_r > 1: raise Exception
+        pos = (new_x, new_y)
+        track_x.append(new_x)
+        track_y.append(new_y)
+ 
+    if kill_photon:
+      if debug: print ' killed photon'
+      continue
 
-      if debug_hardcore:
-        with open('local','w') as f:
-          f.write('%f\t%f\t%f\t%f\n' % (event_x, event_y, ray_len*math.cos(theta), ray_len*math.sin(theta)))
-        with open('global','w') as f:
-          f.write('%f\t%f\t%f\t%f\n' % (0.0, 0.0, math.cos(angle_to_pmt), math.sin(angle_to_pmt)))
-        print 'event', event
-        print ' local: ', event_x, event_y, ray_len*math.cos(theta), ray_len*math.sin(theta)
-        print ' global:', 0.0, 0.0, ray_len*math.cos(angle_to_pmt), ray_len*math.sin(angle_to_pmt)
+    pmtid = int(math.floor((angle_to_pmt+math.pi)/(2*math.pi) * npmts))
+    if debug: print ' hit pmt', pmtid, math.degrees(angle_to_pmt)
 
-      pmt = int(math.floor((angle_to_pmt+math.pi)/(2*math.pi) * npmts))
-      #print pmt, ray_len, theta, angle_to_pmt
-      if debug:
-        print 'pmt:', pmt
+    event.pmts[pmtid].npe += 1
 
-      hits_temp[pmt] += 1
+    event.pmts[pmtid].q += 1.0 #max(pick_charge(), 0.0)
 
-      ch = pick_charge()
-      if debug:
-        print 'q =', ch,
-        if ch < 0:
-          raise Exception
-      q_temp[pmt] += ch
-      # first photon time
-      if (ray_len < t_temp[pmt]) or (t_temp[pmt] == 0.0):
-        if debug: print 't =', abs(ray_len)
-        t_temp[pmt] = abs(ray_len)
-      else:
-        if debug: print
+    # earliest photon time
+    if (ray_len < event.pmts[pmtid].t) or (event.pmts[pmtid].t == 0.0):
+      event.pmts[pmtid].t = abs(ray_len)
 
-      if debug:
-        print q_temp
-        print t_temp
+  return event, track_x, track_y
 
-    for pmt in range(npmts):
-      if hits_temp[pmt] > 0:
-        mean_pe = q_temp[pmt]/hits_temp[pmt]
-        q[pmt].append(mean_pe)
-        t[pmt].append(t_temp[pmt])
-        hits[pmt] += hits_temp[pmt]
-      else:
-        q[pmt].append(0.0)
-        t[pmt].append(0.0)
+def spectrum_flat():
+  return 1000
 
-  return hits, q, t
+def test_scattering():
+  position = (0.5, 0.0)
+  attenuation_length = 100 * radius
+  for j in [100, 1, 0.1, 0.01, 0.001]:
+    exec 'scattering_length = ' + str(j) in globals()
+    ev, tx, ty = simulate(position, spectrum_flat())
+    plot_hit_distribution_polar(ev, pos=position)
+  plt.show()
+
+def test_absorption():
+  position = (0.5, 0.0)
+  for j in [10, 1, 0.5, 0.1, 0.01]:
+    exec 'attenuation_length = ' + str(j) in globals()
+    ev, tx, ty = simulate(position, spectrum_flat())
+    plot_hit_distribution_polar(ev, pos=position)
+  plt.show()
 
 # main
 if __name__ == '__main__':
-  #position = (-0.75, 0.0)
-  #position = 'random'
-  nevents = 1000
-  photons_per_event = 500 # use fn w energy spectrum
+  test_scattering()
 
-  charge = []
-  time = []
-  hits = []
+  #events = []
+  #for position in [(-0.5, 0.0), (0.0, 0.0), (0.5, 0.0)]:
+  #  print '**', position, '***'
+  #  for ev in range(100):
+  #    ev, tx, ty = simulate(position, spectrum_flat())
+  #    plot_hit_distribution_polar(ev, pos=position)
+  #    events.append(ev)
 
-  for position in [(-0.5, 0.0), (0.0, 0.0), (0.5, 0.0)]:
-    print '**', position, '***'
-    h, q, t = simulate(nevents, position, photons_per_event)
-    plot_hit_distribution_polar(h, pos=position)
-    q_vs_t_all(q, t, pos=position)
-    hits.append(h)
-    charge.append(q)
-    time.append(t)
+  #histogram_nhit(events)
+  #histogram_q(events)
+  #histogram_qpe(events)
 
-  # output
-  if debug:
-    print '** summary ****'
-    if debug_hardcore:
-      print q
-      print t
-    print hits
-
-  #histogram_mean_pe_all(q)
-  #histogram_time_all(t)
-
-  plt.show()
+  #plt.show()
 
